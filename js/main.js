38c3a4f7e468bb6f9ab76839507dedf351e0904b
Viktor27 <kolesnik.27091993@gmail.com>
2017-01-04 15:43:29 +0200

    Shame

---
+++ js/main.js
@@ -0,0 +1,18 @@
+;(function($){
+
+	"Use strict"
+
+	$(function(){
+		var slider = $('.ba-slider');
+
+		slider.slick({
+			dots: true,
+			vertical: true,
+			verticalSwiping: true,
+			infinite: false
+		});
+
+
+	});
+
+})(jQuery);
